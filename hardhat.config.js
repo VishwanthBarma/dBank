b82ad8fc253947b53a3c3522bf2debaaa698b2d9
Barma Vishwanth <vishwanthbsavp@gmail.com>
2022-10-24 02:57:49 +0530

    hardhat

---
+++ hardhat.config.js
@@ -0,0 +1,15 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  networks: {
+    hardhat: {},
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${process.env.PROJECT_ID}`,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    mainnet: {
+      url: `https://polygon-mainnet.infura.io/v3/${process.env.PROJECT_ID}`,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  solidity: "0.8.17",
+};
