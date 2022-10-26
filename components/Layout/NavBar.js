d391741ffc9e75ff40336a54a7ee1e76513ea711
Barma Vishwanth <vishwanthbsavp@gmail.com>
2022-10-27 00:31:59 +0530

    navbar init

---
+++ components/Layout/NavBar.js
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import React from "react";
+import { BiSearch } from "react-icons/bi";
+import { TbMinusVertical } from "react-icons/tb";
+
+function NavBar() {
+  return (
+    <div className="bg-neutral-800 h-14 flex p-2 items-center px-8">
+      <h1 className="text-pink-500 font-bold text-2xl">Logo</h1>
+      <div className="bg-neutral-900 h-10 flex-1 mx-10 rounded-lg flex items-center px-3">
+        <BiSearch className="h-7 w-7 text-slate-400" />
+        <TbMinusVertical className="h-9 w-9 text-slate-400" />
+        <input
+          className="bg-transparent flex-1 outline-none text-slate-100 font-bold"
+          placeholder="search..."
+        ></input>
+      </div>
+      <div className="flex items-center space-x-5">
+        <Link href="/explore">
+          <a className="font-bold">Explore</a>
+        </Link>
+        <Link href="/create">
+          <a className="font-bold">Create</a>
+        </Link>
+        <div className="h-10 w-10 rounded-full bg-yellow-500"></div>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
